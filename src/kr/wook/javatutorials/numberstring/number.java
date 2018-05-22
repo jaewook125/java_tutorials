703af76c5cc27aa09be4215f49b1bdef0d2fbc97
jaewook125 <sjw0484@naver.com>
2018-05-22 16:31:12 +0900

    int

--- src/kr/wook/javatutorials/numberstring/number.java
+++ src/kr/wook/javatutorials/numberstring/number.java
@@ -6,6 +6,8 @@ public class number {
 		// TODO Auto-generated method stub
 		System.out.println(1+2);
 		System.out.println(1.23+1.17);
+		System.out.println(2*4);
+		System.out.println(2/4);
 	}
 
 }
